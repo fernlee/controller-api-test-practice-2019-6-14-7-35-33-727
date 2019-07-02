653ff5bd0662e450eb928305fac992abc09f3ea7
yaeli <yaeli@thoughtworks.com>
2019-07-02 23:46:48 +0800

    [yae][add tests for saveTodo method in TodoController]

--- src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
+++ src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
@@ -1,5 +1,6 @@
 package com.tw.api.unit.test.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tw.api.unit.test.domain.todo.Todo;
 import com.tw.api.unit.test.domain.todo.TodoRepository;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,8 +18,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -85,16 +90,43 @@ class TodoControllerTest {
         result.andExpect(status().isNotFound());
     }
 
-//    @Test
-//    void saveTodo() {
-//    }
-//
+    @Test
+    void saveTodo() throws Exception {
+        //given
+
+        final Todo todo = new Todo("Remove unused imports", true);
+        List<Todo> todos = Arrays.asList(todo);
+        final String jsonContent = "{title: 'Remove unused imports','completed': true}";
+
+        when(todoRepository.getAll()).thenReturn(todos);
+
+        //when
+        ResultActions result = mvc.perform(post("/todos")
+                .content(asJsonString(todo))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        result.andExpect(status().isCreated())
+                .andDo(print())
+                .andExpect(content().json(jsonContent));
+    }
+
 //    @Test
 //    void deleteOneTodo() {
+//
 //    }
 //
 //    @Test
 //    void updateTodo() {
 //    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
